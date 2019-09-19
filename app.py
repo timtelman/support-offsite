c35c0b30258039dd7ec2f55ea4e605df07216177
Kat Cosgrove <katc@jfrog.com>
2019-09-19 14:22:26 -0700

    testing rainbow

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 from scripts.pulse import pulse
 from scripts.rainbow import rainbow
-from scripts.weather import weather
-from scripts.twitter import twitter
+# from scripts.weather import weather
+# from scripts.twitter import twitter
 import blinkt
 
 def lights():
-    effect = twitter
+    effect = rainbow
     effect()
 
 if __name__ == '__main__':
